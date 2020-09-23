cc7516fa00319747780cb97befa6ca51d3003954
pratikshit vashistha <50011440+pratikshitvas@users.noreply.github.com>
2020-09-24 00:26:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,97 +1,219 @@
 import pyttsx3
-import speech_recognition as sr
 import datetime
+import speech_recognition as sr
 import wikipedia
-import webbrowser
-import os
 import smtplib
+import webbrowser as wb
+import psutil
+import pyjokes
+import os
+import pyautogui
+import wolframalpha
+import json
+import requests
+from urllib.request import urlopen
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
 
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour< 12:
-        speak("Good morning!")
-    elif hour >= 12 and hour <18:
-        speak("Good afternoon")
-    else:
-        speak("Good evening")
+engine=pyttsx3.init()
 
-    speak("I am Jarvis Mam, Please tell me how may I help you")
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def time_():
+    Time=datetime.datetime.now().strftime("%H:%M:%S")
+    speak("the currnt time is")
+    speak(Time)
+def date_():
+    year=datetime.datetime.now().year
+    month=datetime.datetime.now().month
+    date=datetime.datetime.now().day
+    speak('the current date is')
+    speak(date)
+    speak(month)
+    speak(year)
+
+
+def wishme():
+    speak("hello sir. welcome back !")
+    time_()
+    date_()
+
+    hour=datetime.datetime.now().hour
+    if hour>=6 and hour<12:
+        speak("good morning sir!")
+    elif hour >=12 and hour<18:
+        speak(" good afteroon sir!")
+    elif hour >=18 and hour<20:
+        speak("good evening sir!")
+    elif hour>=20 and hour<24:
+        speak("sir its time to go to sleep. you may go to sleep")
+    else:
+        speak("sir its very late you should sleep.")
+
+    speak(". what we will be doing now sir!")
+
 def takeCommand():
-    #It takes microscope input from the user and returns string output
-    r = sr.Recognizer()
+    r=sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
+        r.pause_threshold=1
+        audio=r.listen(source)
     try:
         print("Recognizing...")
-        query = r.recognize_google(audio, language= 'en-in')
-        print(f"User said:{query}\n")
-
+        query=r.recognize_google(audio,language='en-US')
+        print(query)
     except Exception as e:
         print(e)
-        print("Say that again please...")
-        return "None"
+        print("Sir i cant get it, please say it again")
+        return
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com',587)
+def sendEmail(to,content):
+    server=smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
-    server.login('youremailid', 'password')
-    server.sendmail('youremailid',to, content)
+
+    server.login('username@gmail.com','password')
+    server.sendmail('sender_mail',to,content)
     server.close()
+def cpu():
+    usage=str(psutil.cpu_percent())
+    speak('CPU is at'+usage)
+    battery=psutil.sensors_battery()
+    speak('battery is')
+    speak(battery.percent +'charged')
+
+def joke():
+    speak(py.jokes.get_joke())
+
+def screenshot():
+    img=pyautogui.screenshot()
+    img.save('C:/Users/Public/Pictures/screenshotAI.png')
 
 if __name__ == "__main__":
-    wishMe()
-    # while True:
-    if 1:
-        query = takeCommand().lower()
-        #logic for executing taska based on query
-        if 'wikipedia' in query:
-            speak('Searching wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences = 2)
-            speak('According to Wikipedia')
-            print(results)
-            speak(results)
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
 
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-        
-        elif 'open icloud' in query:
-            webbrowser.open("https://gu.icloudems.com/corecampus/index.php")
-            speak("Login to your icloud account to proceed")
-            
 
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Mam, the time is {strTime}")
+    wishme()
 
-        elif 'open code' in query:
-            codePath ="C:\\Users\\varun\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-
-        elif 'email to me' in query:
+    while True:
+        query=takeCommand()
+        if 'time ' in query:
+            time_()
+        elif 'date' in query:
+            date_()
+        elif 'wikipedia' in query:
+            speak('Searching.....')
+            query=query.replace('wikipedia','')
+            result=wikipedia.summary(query,sentences=5)
+            speak('According to wikipedia, ')
+            print(result)
+            speak(result)
+        elif 'send Email' in query:
             try:
-                speak("What shoul I say")
-                content  = takeCommand()
-                to = "youremailid"
+                speak("what should i write ?")
+                content=takeCommand()
+                speak("please enter the recivers email_id")
+                reciver=input('enter email id:')
+                to=reciver
                 sendEmail(to,content)
-                speak("email has been sent")
-
+                speak(content)
+                speak('email is sent to')
             except Exception as e:
                 print(e)
-                speak("Sorry my friend nandini. I am not able to send this email")
+                speak("mail cant be send")
+        elif 'open website' in query:
+            speak('which website to open?')
+            chromepath = 'c:/Program files (x86)/Google/Application/chrome.exe %s'
+            search=takeCommand()
+            wb.get(chromepath).open_new_tab(search+'.com')
+
+        elif 'search youtube' in query:
+            speak("what do you want to play on youtube?")
+            search_Term=takeCommand()
+            speak("this is youtube related")
+            wb.open('http://www.youtube.com/result?search_query='+search_Term)
+
+        elif 'cpu'in query:
+            cpu()
+
+        elif 'joke' in query:
+            joke()
+
+        elif 'go to sleep' in query:
+            speak('have a good day sir. BYe sir')
+            quit()
+
+        elif 'Word' in query:
+            speak('opening MS word...')
+            ms_word= r'C:\Program Files (x86)\Microsoft Office\Office12\WINWORD'
+            os.startfile(ms_word)
+
+        elif 'Powerpoint' or 'ppt' in query:
+            speak('opening MS powerpoint presentation ...')
+            ms_ppt= r'C:\Program Files (x86)\Microsoft Office\Office12\POWERPNT'
+            os.startfile(ms_ppt)
+
+        elif 'Excel' in query:
+            speak('opening MS excel...')
+            ms_excel= r'C:\Program Files (x86)\Microsoft Office\Office12\EXCEL'
+            os.startfile(ms_excel)
+
+        elif 'Write a note' in search_query:
+            speak('what should i write, sir?')
+            notes =takeCommand()
+            file =open('notes.txt','w')#create your own file in your system where you save this file
+            speak('Sir should i include Date and Time? ')
+            ans=takeCommand()
+            if 'yes' in ans or 'sure' in ans:
+                strTime=datetime.datetime.now().strftime('%H:%M:%S')
+                file.write(strTime)
+                file.write(":-")
+                file.write(notes)
+                speak('done taking notes sir.')
+            else:
+                file.write(notes)
+        elif 'Show notes' in query:
+            speak('showing notes')
+            file=open('notes.txt','r')
+            print(file.read())
+            speak(file.read())
+
+        elif 'Screenshot' in query:
+            screenshot()
+
+        #elif 'Play music' in query:
+
+        elif 'what is' in query or 'who is' in query:
+            client=wolframalpha.Client('8EW922-36E8QQYRJT')
+            res = client.query(query)
+
+            try:
+                print(next(res.result).text)
+                speak(next(res.result).text)
+            except:
+                print("not found")
+        elif 'Make a reminder' in query:
+            speak("what should i remind you , sir!")
+            memory=takeCommand()
+            speak("i made reminder"+memory)
+            reminder=open('memory.txt','w')
+            reminder.write(memory)
+            reminder.close()
+
+        #setting a reminder
+        elif 'News' in query:
+            try:
+                jsonObj=urlopen("http://newsapi.org/v2/top-headlines?country=india&category=entertainment&apiKey=32c7d7ec660744669c2719430e560f8f")
+                i=1
+                speak('here are some top hadline from the entertainmemnt industry')
+                print('***************TOP HEADLINE***************'+'\n')
+                for item in data['article']:
+                    print(str(i)+'.'+item['title']+'\n')
+                    print(item['description']+'\n')
+                    speak(item['title'])
+                    i+=1
+
+            except Exception as e:
+                    print(str(e))
