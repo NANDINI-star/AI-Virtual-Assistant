fa74cb112b2e4eb8626bca82ebe00c694d48ddb9
Nandini Bagga <bagganandini8@gmail.com>
2020-10-03 22:34:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import pyttsx3
-import datetime
 import speech_recognition as sr
+import datetime
 import wikipedia
+import webbrowser
+import os
 import smtplib
 from selenium import webdriver
 import time
+<<<<<<< HEAD
 import webbrowser as wb
 import psutil
 import pyjokes
@@ -14,7 +17,12 @@ import wolframalpha
 import json
 import requests
 from urllib.request import urlopen
+=======
+>>>>>>> parent of 90944df... Merge branch 'master' of https://github.com/NANDINI-star/AI-Virtual-Assistant
 
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
 
 class GUicloudbot:
 
@@ -49,16 +57,21 @@ class GUicloudbot:
 #     else:
 #         speak("Good evening")
 
+<<<<<<< HEAD
 #     speak("How can I help you")
 
 engine=pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+=======
+    speak("How can I help you")
+>>>>>>> parent of 90944df... Merge branch 'master' of https://github.com/NANDINI-star/AI-Virtual-Assistant
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+<<<<<<< HEAD
 def time_():
     Time=datetime.datetime.now().strftime("%H:%M:%S")
     speak("the current time is")
@@ -92,14 +105,19 @@ def wishMe():
 
     speak("How can I help you?")
 
+=======
+>>>>>>> parent of 90944df... Merge branch 'master' of https://github.com/NANDINI-star/AI-Virtual-Assistant
 def takeCommand():
-    r=sr.Recognizer()
+    #It takes microscope input from the user and returns string output
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold=1
-        audio=r.listen(source)
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
     try:
         print("Recognizing...")
+<<<<<<< HEAD
         query=r.recognize_google(audio,language='en-in')
         print(f"User said:{query}\n")
     except Exception as e:
@@ -107,11 +125,22 @@ def takeCommand():
         print("mam i cant get it, please say it again")
         return "None"
     return query
+=======
+        query = r.recognize_google(audio, language= 'en-in')
+        print(f"User said:{query}\n")
 
-def sendEmail(to,content):
-    server=smtplib.SMTP('smtp.gmail.com',587)
+    except Exception as e:
+        print(e)
+        print("Say that again please...")
+        return "None"
+    return query    
+>>>>>>> parent of 90944df... Merge branch 'master' of https://github.com/NANDINI-star/AI-Virtual-Assistant
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
+<<<<<<< HEAD
     server.login('username@gmail.com','password')
     server.sendmail('sender_mail',to,content)
     server.close()
@@ -129,6 +158,11 @@ def joke():
 def screenshot():
     img=pyautogui.screenshot()
     img.save('C:/Users/Public/Pictures/screenshotAI.png')
+=======
+    server.login('youremailid', 'password')
+    server.sendmail('youremailid',to, content)
+    server.close()
+>>>>>>> parent of 90944df... Merge branch 'master' of https://github.com/NANDINI-star/AI-Virtual-Assistant
 
     
 
@@ -183,102 +217,8 @@ if __name__ == "__main__":
                 content  = takeCommand()
                 to = "youremailid"
                 sendEmail(to,content)
-                speak(content)
-                speak('email is sent to')
+                speak("email has been sent")
+
             except Exception as e:
                 print(e)
-                speak("mail cant be send")
-        elif 'open website' in query:
-            speak('which website to open?')
-            chromepath = 'c:/Program files (x86)/Google/Application/chrome.exe %s'
-            search=takeCommand()
-            wb.get(chromepath).open_new_tab(search+'.com')
-
-        elif 'search youtube' in query:
-            speak("what do you want to play on youtube?")
-            search_Term=takeCommand()
-            speak("this is youtube related")
-            wb.open('http://www.youtube.com/result?search_query='+search_Term)
-
-        elif 'cpu'in query:
-            cpu()
-
-        elif 'joke' in query:
-            joke()
-
-        elif 'go to sleep' in query:
-            speak('have a good day sir. BYe sir')
-            quit()
-
-        elif 'Word' in query:
-            speak('opening MS word...')
-            ms_word= r'C:\Program Files (x86)\Microsoft Office\Office12\WINWORD'
-            os.startfile(ms_word)
-
-        elif 'Powerpoint' or 'ppt' in query:
-            speak('opening MS powerpoint presentation ...')
-            ms_ppt= r'C:\Program Files (x86)\Microsoft Office\Office12\POWERPNT'
-            os.startfile(ms_ppt)
-
-        elif 'Excel' in query:
-            speak('opening MS excel...')
-            ms_excel= r'C:\Program Files (x86)\Microsoft Office\Office12\EXCEL'
-            os.startfile(ms_excel)
-
-        elif 'Write a note' in search_query:
-            speak('what should i write, sir?')
-            notes =takeCommand()
-            file =open('notes.txt','w')#create your own file in your system where you save this file
-            speak('Sir should i include Date and Time? ')
-            ans=takeCommand()
-            if 'yes' in ans or 'sure' in ans:
-                strTime=datetime.datetime.now().strftime('%H:%M:%S')
-                file.write(strTime)
-                file.write(":-")
-                file.write(notes)
-                speak('done taking notes sir.')
-            else:
-                file.write(notes)
-        elif 'Show notes' in query:
-            speak('showing notes')
-            file=open('notes.txt','r')
-            print(file.read())
-            speak(file.read())
-
-        elif 'Screenshot' in query:
-            screenshot()
-
-        #elif 'Play music' in query:
-
-        elif 'what is' in query or 'who is' in query:
-            client=wolframalpha.Client('8EW922-36E8QQYRJT')
-            res = client.query(query)
-
-            try:
-                print(next(res.result).text)
-                speak(next(res.result).text)
-            except:
-                print("not found")
-        elif 'Make a reminder' in query:
-            speak("what should i remind you , sir!")
-            memory=takeCommand()
-            speak("i made reminder"+memory)
-            reminder=open('memory.txt','w')
-            reminder.write(memory)
-            reminder.close()
-
-        #setting a reminder
-        elif 'News' in query:
-            try:
-                jsonObj=urlopen("http://newsapi.org/v2/top-headlines?country=india&category=entertainment&apiKey=32c7d7ec660744669c2719430e560f8f")
-                i=1
-                speak('here are some top hadline from the entertainmemnt industry')
-                print('***************TOP HEADLINE***************'+'\n')
-                for item in data['article']:
-                    print(str(i)+'.'+item['title']+'\n')
-                    print(item['description']+'\n')
-                    speak(item['title'])
-                    i+=1
-
-            except Exception as e:
-                    print(str(e))
+                speak("Sorry my friend nandini. I am not able to send this email")
